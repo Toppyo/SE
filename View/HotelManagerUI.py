98d2338b3c79ce9c7dced49c766c99c050d07d3d
yuminhao <yu@pano-tech.com>
2018-02-01 16:55:19 +0900

    213

--- View/HotelManagerUI.py
+++ View/HotelManagerUI.py
@@ -1,59 +1,39 @@
-from utils import utils
 import sys
+from Controller.HotelManagementProcessor import HotelManagementProcessor
 
 class HotelManagerUI():
 
     def __init__(self):
         # self.password = "awesome"
-        self.util = utils()
+        self.hmp = HotelManagementProcessor()
         self.warning = {}
         self.warning['format'] = "Wrong input format "
         self.warning['date'] = "Date input doesn't exist "
         self.warning['missing'] = "Missing search filter of "
         self.warning['signin'] = "You don't have the permission of manager, please check clientUI instead "
         # self.warning['password'] = "Wrong password for manager-sign-in "
-        self.menu = "Please input number to give command\n" \
-                    "1-->show all rooms\n" \
-                    "2-->show rooms by date\n" \
-                    "3-->show all clients\n" \
-                    "4-->show client by client number\n" \
-                    "5-->register a user to the system\n" \
-                    "6-->input reservation number to check-in\n" \
-                    "7-->input reservation number to check-out\n" \
-                    "8-->exit"
-
-    def showAllRoom(self):
-        # TODO
-        return
-
-    def showAllRoomByDate(self):
-        # TODO
-        return
-
-    def showAllClient(self):
-        # TODO
-        return
-
-    def showClientByClientNumber(self):
-        # TODO
-        return
+        self.menu = "Please input number to give command\n"  \
+                    "1-->register a user to the system\n" \
+                    "2-->input reservation number to check-in\n" \
+                    "3-->input reservation number to check-out\n" \
+                    "4-->exit"
 
     def userRegister(self):
         name = input("Please input name for registration: \n")
         level = input("Please input level for registration: \n")
-        _continue = self.util.check_int(level) and int(level)<4
+        _continue = self.hmp.check_int(level) and int(level)<4
         while(not _continue):
             self.showWarning("format")
             level = input("Please input level for registration: \n")
-            _continue = self.util.check_int(level) and int(level)<4
+            _continue = self.hmp.check_int(level) and int(level)<4
         # TODO register(name, level)
-        self.util.normalPrint("Registration for " + name + " san succeed!")
+        self.hmp.normalPrint("Registration for " + name + " san succeed!")
         return
 
     def inputSignInInfo(self):
         manager_num = input("Please sign in your manager number: \n").strip()
-        if (self.util.check_int(manager_num)):
-            client_level = self.util.check_client(int(manager_num))[0]
+        if (self.hmp.check_int(manager_num)):
+            client_level = self.hmp.check_client(int(manager_num))[0]
             if client_level == 3:
                 # TODO self.client = client(client_num)
                 # TODO self.util.normalPrint("Welcome " + manager.name)
@@ -67,8 +47,8 @@ class HotelManagerUI():
 
     def inputReservationNumberForCheckIn(self):
         reservation_num = input("Please input reservation number: \n").strip()
-        if(self.util.check_int(reservation_num)):
-            # TODO checkin(reservation_num)
+        if(self.hmp.check_int(reservation_num)):
+            self.hmp.checkin_by_reservation(int(reservation_num))
             return
         else:
             self.showWarning("format")
@@ -76,8 +56,8 @@ class HotelManagerUI():
 
     def inputReservationNumberForCheckOut(self):
         reservation_num = input("Please input reservation number: \n").strip()
-        if (self.util.check_int(reservation_num)):
-            # TODO checkout(reservation_num)
+        if (self.hmp.check_int(reservation_num)):
+            self.hmp.checkout_by_reservation(int(reservation_num))
             return
         else:
             self.showWarning("format")
@@ -86,35 +66,27 @@ class HotelManagerUI():
         pass
 
     def showWarning(self, mode, addition = ""):
-        self.util.warningPrint(self.warning[mode] + addition + "!")
+        self.hmp.warningPrint(self.warning[mode] + addition + "!")
 
     def mainMenu(self):
         while(True):
-            self.util.normalPrint(self.menu)
+            self.hmp.normalPrint(self.menu)
             command = input("Input: ")
-            if (self.util.check_int(command)):
+            if (self.hmp.check_int(command)):
                 command = int(command)
                 if (command == 1):
-                    self.showAllRoom()
-                elif (command == 2):
-                    self.showAllRoomByDate()
-                elif (command == 3):
-                    self.showAllClient()
-                elif (command == 4):
-                    self.showClientByClientNumber()
-                elif (command == 5):
                     self.userRegister()
-                elif (command == 6):
+                elif (command == 2):
                     self.inputReservationNumberForCheckIn()
-                elif (command == 7):
+                elif (command == 3):
                     self.inputReservationNumberForCheckOut()
-                elif (command == 8):
+                elif (command == 4):
                     break
                 else:
                     self.showWarning("format")
             else:
                 self.showWarning("format")
-        self.util.normalPrint("Thank you for using!")
+        self.hmp.normalPrint("Thank you for using!")
         sys.exit(0)
 
 if __name__ == '__main__':
--- View/RoomReservationUI.py
+++ View/RoomReservationUI.py
@@ -1,9 +1,11 @@
-from utils import utils
 import sys
+from Controller.RoomReservationProcessor import RoomReservationProcessor
+from Controller.Calendar import calendar
 
 class RoomReservationUI():
     def __init__(self):
-        self.util = utils()
+        self.rrp = RoomReservationProcessor()
+        self.calendar = calendar()
         self.date = []
         self.type = None
         self.warning = {}
@@ -31,46 +33,46 @@ class RoomReservationUI():
 
     def inputDateSearchCondition(self):
         year = input("Please input year: ").strip()
-        if(self.util.check_int(year)):
+        if(self.rrp.check_int(year)):
             self.year = int(year)
-            self.util.createCalendar(self.year)
+            self.calendar.createCalendar(self.year)
         else:
             self.showWarning("format")
             return
         month = input("Please input month: ").strip()
-        _continue = self.util.check_int(month) and int(month) in self.util.calendar_dict.keys()
+        _continue = self.rrp.check_int(month) and int(month) in self.calendar.calendar_dict.keys()
         while(not _continue):
-            if(not self.util.check_int(month)):
+            if(not self.rrp.check_int(month)):
                 self.showWarning("format")
             else:
                 self.showWarning("date")
             month = input("Please input month: ").strip()
-            _continue = self.util.check_int(month) and int(month) in self.util.calendar_dict.keys()
+            _continue = self.rrp.check_int(month) and int(month) in self.calendar.calendar_dict.keys()
         self.month = int(month)
         day = input("Please input day: ").strip()
-        _continue = self.util.check_int(day) and int(day) in self.util.calendar_dict[self.month]
+        _continue = self.rrp.check_int(day) and int(day) in self.calendar.calendar_dict[self.month]
         while (not _continue):
-            if (not self.util.check_int(day)):
+            if (not self.rrp.check_int(day)):
                 self.showWarning("format")
             else:
                 self.showWarning("date")
             day = input("Please input day: ").strip()
-            _continue = self.util.check_int(day) and int(day) in self.util.calendar_dict[self.month]
+            _continue = self.rrp.check_int(day) and int(day) in self.calendar.calendar_dict[self.month]
         self.day = int(day)
-        toAdd = str(self.year) + self.util.num2str(self.month) + self.util.num2str(self.day)
+        toAdd = str(self.year) + self.rrp.num2str(self.month) + self.rrp.num2str(self.day)
         self.date.append(toAdd)
-        self.util.normalPrint("Setting of SearchDate: " + toAdd + " succeed!")
+        self.rrp.normalPrint("Setting of SearchDate: " + toAdd + " succeed!")
         return
 
     def inputTypeSearchCondition(self):
         type = input("Please input room type (D for Double and S for Single): ").strip()
         if (type == "D" or type == "Double"):
             self.type = "Double"
-            self.util.normalPrint("Setting of SearchType: " + self.type + " succeed!")
+            self.rrp.normalPrint("Setting of SearchType: " + self.type + " succeed!")
             return
         elif (type == "S" or type == "Single"):
             self.type = "Single"
-            self.util.normalPrint("Setting of SearchType: " + self.type + " succeed!")
+            self.rrp.normalPrint("Setting of SearchType: " + self.type + " succeed!")
             return
         else:
             self.showWarning("format")
@@ -82,36 +84,36 @@ class RoomReservationUI():
             return
         else:
             year = input("Please input year: ").strip()
-            if (self.util.check_int(year)):
+            if (self.rrp.check_int(year)):
                 self.year = int(year)
-                self.util.createCalendar(year)
+                self.calendar.createCalendar(year)
             else:
                 self.showWarning("format")
                 return
             month = input("Please input month: ").strip()
-            _continue = self.util.check_int(month) and int(month) in self.util.calendar_dict.keys()
+            _continue = self.rrp.check_int(month) and int(month) in self.calendar.calendar_dict.keys()
             while (not _continue):
-                if (not self.util.check_int(month)):
+                if (not self.rrp.check_int(month)):
                     self.showWarning("format")
                 else:
                     self.showWarning("date")
                 month = input("Please input month: ").strip()
-                _continue = self.util.check_int(month) and int(month) in self.util.calendar_dict.keys()
+                _continue = self.rrp.check_int(month) and int(month) in self.calendar.calendar_dict.keys()
             self.month = int(month)
             day = input("Please input day: ").strip()
-            _continue = self.util.check_int(day) and int(day) in self.util.calendar_dict[self.month]
+            _continue = self.rrp.check_int(day) and int(day) in self.calendar.calendar_dict[self.month]
             while (not _continue):
-                if (not self.util.check_int(day)):
+                if (not self.rrp.check_int(day)):
                     self.showWarning("format")
                 else:
                     self.showWarning("date")
                 day = input("Please input day: ").strip()
-                _continue = self.util.check_int(day) and int(day) in self.util.calendar_dict[self.month]
+                _continue = self.rrp.check_int(day) and int(day) in self.calendar.calendar_dict[self.month]
             self.day = int(day)
-            toDelete = str(self.year) + self.util.num2str(self.month) + self.util.num2str(self.day)
+            toDelete = str(self.year) + self.rrp.num2str(self.month) + self.rrp.num2str(self.day)
             if toDelete in self.date:
                 self.date.remove(toDelete)
-                self.util.normalPrint("Remove of SearchDate: " + toDelete + " succeed!")
+                self.rrp.normalPrint("Remove of SearchDate: " + toDelete + " succeed!")
                 return
             else:
                 self.showWarning("date", "in search filter ")
@@ -125,8 +127,8 @@ class RoomReservationUI():
             self.showWarning("missing", "type ")
             return
         else:
-            results, self.reservation_num = self.util.add_reservation(self.date, self.type, self.client_num)
-            self.util.normalPrint("Your reservation number for this time is " + str(self.reservation_num) + "\n"
+            results, self.reservation_num = self.rrp.add_reservation(self.date, self.type, self.client_num)
+            self.rrp.normalPrint("Your reservation number for this time is " + str(self.reservation_num) + "\n"
                                   + "The searching result is as follows:\n" + str(results))
             self.showConfirmReservationMessage()
             return
@@ -137,7 +139,7 @@ class RoomReservationUI():
                 self.showWarning("missing", "reservation number ")
                 return
             else:
-                reservation_client, reservation_detail, index_found, check_in = self.util.check_reservation_num(self.reservation_num)
+                reservation_client, reservation_detail, index_found, check_in = self.rrp.check_reservation_num(self.reservation_num)
                 if index_found==-1 or self.client_num!=reservation_client:
                     self.showWarning("notfound", "reservation of reservation number: " + str(self.reservation_num) + " of your account")
                     return
@@ -145,10 +147,10 @@ class RoomReservationUI():
                     toDisplay = ("Reservation number: " + str(reservation_num) + "\n" + "Reservation client: "
                                  + str(reservation_client) + "\n" + "Reservation details: "
                                  + str(reservation_detail) + "\n" + "Reservation state: " + str(check_in) +"\n")
-                    self.util.normalPrint(toDisplay)
+                    self.rrp.normalPrint(toDisplay)
                     return
         else:
-            reservation_client, reservation_detail, index_found, check_in = self.util.check_reservation_num(
+            reservation_client, reservation_detail, index_found, check_in = self.rrp.check_reservation_num(
                 reservation_num)
             if index_found == -1:
                 self.showWarning("notfound", "reservation of reservation number: " + str(reservation_num))
@@ -156,8 +158,8 @@ class RoomReservationUI():
             else:
                 toDisplay = ("Reservation number: " + str(reservation_num) + "\n" + "Reservation client: "
                              + str(reservation_client) + "\n" + "Reservation details: "
-                             + str(reservation_detail) + "\n" + "Reservation state: " + self.util.check_state(check_in) + "\n")
-                self.util.normalPrint(toDisplay)
+                             + str(reservation_detail) + "\n" + "Reservation state: " + self.rrp.check_state(check_in) + "\n")
+                self.rrp.normalPrint(toDisplay)
                 return
 
     def cancelReservation_base(self, reservation_num=None):
@@ -166,19 +168,19 @@ class RoomReservationUI():
                 self.showWarning("missing", "reservation number ")
                 return
             else:
-                succeeded = self.util.cancel_reservation(self.reservation_num, self.client_num)
+                succeeded = self.rrp.cancel_reservation(self.reservation_num, self.client_num)
                 if succeeded:
                     toDisplay = ("Reservation of reservation number: " + str(self.reservation_num) + " canceled!")
-                    self.util.normalPrint(toDisplay)
+                    self.rrp.normalPrint(toDisplay)
                     return
                 else:
                     self.showWarning("failure", "reservation canceling of reservation number: " + str(self.reservation_num))
                     return
         else:
-            succeeded = self.util.cancel_reservation(reservation_num, self.client_num)
+            succeeded = self.rrp.cancel_reservation(reservation_num, self.client_num)
             if succeeded:
                 toDisplay = ("Reservation of reservation number: " + str(reservation_num) + " canceled!")
-                self.util.normalPrint(toDisplay)
+                self.rrp.normalPrint(toDisplay)
                 return
             else:
                 self.showWarning("failure", "reservation canceling of reservation number: " + str(self.reservation_num))
@@ -189,7 +191,7 @@ class RoomReservationUI():
         if reservation_num == "":
             self.checkReservation_base()
         else:
-            if self.util.check_int(reservation_num):
+            if self.rrp.check_int(reservation_num):
                 self.checkReservation_base(int(reservation_num))
             else:
                 self.showWarning("format")
@@ -200,7 +202,7 @@ class RoomReservationUI():
         if reservation_num == "":
             self.cancelReservation_base()
         else:
-            if self.util.check_int(reservation_num):
+            if self.rrp.check_int(reservation_num):
                 self.cancelReservation_base(int(reservation_num))
             else:
                 self.showWarning("format")
@@ -210,16 +212,16 @@ class RoomReservationUI():
         pass
 
     def showWarning(self, mode, addition = ""):
-        self.util.warningPrint(self.warning[mode] + addition + "!")
+        self.rrp.warningPrint(self.warning[mode] + addition + "!")
 
     def showConfirmReservationMessage(self):
         confirmed = input("\nAre you sure to confirm the reservation above? (y for YES and n for NO)\n").strip()
         if(confirmed == 'y'):
-            self.util.normalPrint("Reservation succeed!\nYour reservation number is : " + str(self.reservation_num) + " !")
+            self.rrp.normalPrint("Reservation succeed!\nYour reservation number is : " + str(self.reservation_num) + " !")
             return
         elif(confirmed == 'n'):
             self.cancelReservation_base()
-            self.util.normalPrint("Cancelation of reservation: " + str(self.reservation_num) + ' succeed!')
+            self.rrp.normalPrint("Cancelation of reservation: " + str(self.reservation_num) + ' succeed!')
             return
         else:
             self.showWarning("format")
@@ -228,15 +230,15 @@ class RoomReservationUI():
 
     def inputSignInInfo(self):
         client_num = input("Please sign in your client number: \n").strip()
-        if(self.util.check_int(client_num)):
-            client_level, client_name = self.util.check_client(int(client_num))
+        if(self.rrp.check_int(client_num)):
+            client_level, client_name = self.rrp.check_client(int(client_num))
             if client_level == 0:
                 self.showWarning("signin")
                 sys.exit(0)
             else:
                 # TODO self.client = client(client_num)
-                # TODO self.util.normalPrint("Welcome " + client.name)
-                self.util.normalPrint("Welcome " + client_name + " san!")
+                # TODO self.rrp.normalPrint("Welcome " + client.name)
+                self.rrp.normalPrint("Welcome " + client_name + " san!")
                 self.client_num = int(client_num)
                 self.mainMenu()
         else:
@@ -248,14 +250,14 @@ class RoomReservationUI():
 
     def mainMenu(self):
         while(True):
-            self.util.normalPrint(self.main_menu)
+            self.rrp.normalPrint(self.main_menu)
             command = input("Input command: ").strip()
-            if(self.util.check_int(command)):
+            if(self.rrp.check_int(command)):
                 command = int(command)
                 if(command == 1):
-                    self.util.normalPrint(self.sub_menu)
+                    self.rrp.normalPrint(self.sub_menu)
                     command_sub = input("Input command: ").strip()
-                    if (self.util.check_int(command_sub)):
+                    if (self.rrp.check_int(command_sub)):
                         command_sub = int(command_sub)
                         if command_sub == 1 :
                             self.inputDateSearchCondition()
@@ -275,7 +277,7 @@ class RoomReservationUI():
                     self.showWarning("format")
             else:
                 self.showWarning("format")
-        self.util.normalPrint("Thank you for using!")
+        self.rrp.normalPrint("Thank you for using!")
         sys.exit(0)
 
 if __name__ == '__main__':
